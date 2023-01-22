80c653587b82e7e6900196e08309b15acd8a199b
Igor <klimmm27@yandex.ru>
2023-01-22 15:57:44 +0300

    complete

---
+++ Homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. (без использования Math.Pow)
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+
+Console.WriteLine ("Введите число которое хотите возвести в степень: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите степень: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int number_x = 1;
+
+for (int i=0; i<x; i++)
+ number_x*=number;
+
+Console.WriteLine ("Ответ:" + number_x);
---
+++ Homework2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.WriteLine ("Сейчас мы узнаем сумму всех цифр в числе");
+Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+while (number > 0)
+{
+int num = number % 10;
+number = number / 10;
+sum = sum + num;
+}
+Console.WriteLine("Cумма цифр в числе равна: " + sum);
